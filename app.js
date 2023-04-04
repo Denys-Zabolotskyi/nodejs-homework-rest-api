da8c24366db6c90c2a42393c182676a7faa43cab
Denys-Zabolotskyi <d.zabolotskyi@gmail.com>
2023-04-04 18:51:53 +0300

    hw02-express completed

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const contacts = require("./models/contacts.json"); // make a comment after practice
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
-
-// app.get("/contacts", (req, res) => {
-//   res.json(contacts);
-// }); // make a comment after practice
-
-// app.get("/products", (req, res) => {
-//   res.json([]);
-// }); // make a comment after practice
-
-// app.use((req, res, next) => {
-//   console.log("First midlleware");
-//   next();
-// }); // make a comment after practice
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 
-// app.use(express.json());
+app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
@@ -33,7 +20,8 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,8 @@
-// const { json } = require("express");
 const fs = require("fs/promises");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
-console.log(contactsPath);
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
