bcbcbbc761cf840dbb585cf070a9aedd405fdfa1
Denys-Zabolotskyi <d.zabolotskyi@gmail.com>
2023-04-14 15:49:10 +0300

    adding DB

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Denys:LkJn3EFTMgJ6XAG@cluster0.olek4xl.mongodb.net/phone_book?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => console.log(error.message));
+
+// LkJn3EFTMgJ6XAG
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
